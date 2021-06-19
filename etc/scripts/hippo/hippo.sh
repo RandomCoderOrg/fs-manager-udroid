7ec973d63184c17d8cdd7f7b615e8681742dced4
Sam Alarie <65938569+GxmerSam@users.noreply.github.com>
2021-06-19 23:42:00 +0530

    added scripts

--- etc/scripts/hippo/hippo.sh
+++ etc/scripts/hippo/hippo.sh
@@ -135,3 +135,22 @@ if [ $# -eq 0 ]; then
         # ;;
     esac
 fi
+##
+## FOR ADDING FURTHER CMDS
+##
+
+##Adding a vncserver command
+echo "vncserver -kill :* && tigervncserver -geometry 1280x720 -xstartup /usr/bin/starrxfce4 -listen tcp :1" > /data/data/com.termux/files/usr/varlib/proot-distro/installed-rootfs/hippo/usr/bin/vncserver-start;
+# Making it executable
+chmod +x  /data/data/com.termux/files/usr/varlib/proot-distro/installed-rootfs/hippo/usr/bin/vncserver-start;
+
+# Adding script for sound...
+echo "pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1" > $PREFIX/bin/pulseserver-start
+# Making it executable
+chmod +x $PREFIX/bin/pulseserver-start
+
+# Adding sound to .bashrc
+echo "export PULSE_SERVER=127.0.0.1 >> /data/data/com.termux/files/usr/varlib/proot-distro/installed-rootfs/root/.bashrc
+
+echo "export DISPLAY=:1" >> data/data/com.termux/files/usr/varlib/proot-distro/installed-rootfs/hippo/root/.bashrc
+
