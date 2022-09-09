a3066495a908ffb9d8769bda9fa8b409ccdc0bf3
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-09-09 12:39:47 +0530

    fix paths

--- udroid/test/test_mount.sh
+++ udroid/test/test_mount.sh
@@ -4,8 +4,9 @@ set -e -u
 unset LD_PRELOAD
 
 TERMUX_ROOT="/data/data/com.termux/files"
-PROOT_DLCACHE="$TERMUX_ROOT/usr/var/lib/proot-distro/installed-rootfs"
+PROOT_DLCACHE="$TERMUX_ROOT/usr/var/lib/proot-distro/dlcache"
+PROOTD_DIR="$TERMUX_ROOT/usr/var/lib/proot-distro/installed-rootfs"
+source ../src/proot-utils/proot-utils.sh
 
-source proot-utils/proot-utils.sh
-
-login --path ${PROOT_DLCACHE}/udroid-impish-xfce4 -- /bin/sh
+# works only with udroid-impish-xfce4 for now
+login --path ${PROOTD_DIR}/udroid-impish-xfce4 -- /bin/sh
