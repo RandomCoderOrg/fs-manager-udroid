5756a87ad9f68e79e71667904bab7455980d8519
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-10-27 11:03:03 +0530

    fix symlink paths

--- udroid/src/install_sim.sh
+++ udroid/src/install_sim.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-BIN="fs-manager-udroid/bin"
-INSTALL_DIR="fs-manager-udroid/test"
+BIN="${PREFIX}/bin"
+INSTALL_DIR="${PREFIX}/etc/udroid"
 
 sleep 2
-ln -sv $INSTALL_DIR/udroid.sh $BIN/udroid
+ln -sv $INSTALL_DIR/udroid.sh $BIN/udroid || {
+    echo "Installation failed.."
+    exit 1
+}
