f747b5e587094984753a970d5d780c7cd64aaef1
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-11-01 16:01:02 +0530

    define `RTR` in installer

--- udroid/src/install.sh
+++ udroid/src/install.sh
@@ -3,6 +3,7 @@
 BIN="$PREFIX/bin"
 INSTALL_DIR="${PREFIX}/etc/udroid"
 
+RTR="${PREFIX}/etc/udroid"
 DEFAULT_ROOT="${PREFIX}/var/lib/udroid"
 DEFAULT_FS_INSTALL_DIR="${DEFAULT_ROOT}/installed-filesystems"
 DLCACHE="${DEFAULT_ROOT}/dlcache"
