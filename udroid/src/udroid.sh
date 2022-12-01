ef29244785832ae27750439d0ddb43cc0ba80c05
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-12-01 18:13:15 +0530

    remove line that causes failure in logic

--- udroid/src/udroid.sh
+++ udroid/src/udroid.sh
@@ -35,8 +35,6 @@ fetch_distro_data() {
     URL="https://raw.githubusercontent.com/RandomCoderOrg/udroid-download/main/distro-data.json"
     _path="${RTCACHE}/distro-data.json.cache"
 
-    [[ -f $_path ]] && mv $_path $_path.old
-
     if [[ -f $_path ]]; then
         if ! $offline_mode; then
             g_spin dot "Fetching distro data.." curl -L -s -o $_path $URL || {
