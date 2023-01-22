a7ab9753c5bccae51e91e1b73465044a3fe10d49
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-01-22 13:18:20 +0530

    fix: syntax missing $

--- udroid/src/udroid.sh
+++ udroid/src/udroid.sh
@@ -608,7 +608,7 @@ login() {
         fi
 
         # [CONDITIONAL]: pulseaudio server for audio output (speaker)
-        if ! no_pulseserver; then
+        if ! $no_pulseserver; then
             pulseaudio  --start \
                         --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
                         --exit-idle-time=-1
